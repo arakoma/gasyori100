259a05b7919571253803d6479f72107df1cb2865
arakoma <arakoma555@gmail.com>
2019-12-30 21:11:31 +0900

    add 01

---
+++ q_01_10/ans_01.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+img = cv2.imread("imori.jpg")
+
+img[:, :, :] = img[:, :, (2, 1, 0)]
+
+cv2.imwrite("out_01.jpg", img)
+cv2.imshow("", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+#関数として書く方がいいかも
+"""
+def BGR2RGB(img):
+    b = img[:, :, 0]
+    g = img[:, :, 1]
+    r = img[:, :, 2]
+
+    img[:, :, 0] = r
+    img[:, :, 1] = g
+    img[:, :, 2] = b
+
+    return img
+"""
