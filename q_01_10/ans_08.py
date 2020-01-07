f8b6dd792c78decbedb419cba05acbb52d64e31e
arakoma <arakoma555@gmail.com>
2020-01-08 01:14:12 +0900

    add 08 09

---
+++ q_01_10/ans_08.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+
+#n*n画素ごとに分割する or s*s個に分割する
+def max_pooling(img, n=0, s=0):
+    out = img.copy()
+    h, w, c = img.shape
+
+    if n:
+        gy = h // n
+        gx = w // n
+        for i in range(gy):
+            for j in range(gx):
+                for k in range(c):
+                    out[n*i:n*(i+1), n*j:n*(j+1), k] \
+                        = np.max(out[n*i:n*(i+1), n*j:n*(j+1), k])
+    elif s:
+        dy = h // s
+        dx = w // s
+        for i in range(s):
+            for j in range(s):
+                for k in range(c):
+                    out[dy*i:dy*(i+1), dx*j:dx*(j+1), k] \
+                        = np.max(out[dy*i:dy*(i+1), dx*j:dx*(j+1), k])
+    
+    return out
+
+
+img = cv2.imread("imori.jpg").astype(np.float32)
+
+#8*8画素ごとに分割
+out1 = max_pooling(img, n=8).astype(np.uint8)
+
+#8*8個に分割
+out2 = max_pooling(img, s=8).astype(np.uint8)
+
+cv2.imwrite(r"out/out_08_1.jpg", out1)
+cv2.imshow("", out1)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+cv2.imwrite(r"out/out_08_2.jpg", out2)
+cv2.imshow("", out2)
+cv2.waitKey(0)
+cv2.destroyAllWindows
---
+++ q_01_10/ans_09.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+
+
+def gaussian_filter(img_, k_size, s):
+    if len(img_.shape) == 3:
+        img = img_.copy()
+    if len(img.shape) == 2:
+        img = np.expand_dims(img_, -1)
+
+    H, W, C = img.shape    
+    pad = k_size // 2
+
+    #padding
+    img_pad = np.zeros((H+pad*2, W+pad*2, C)).astype(np.float32)
+    img_pad[pad:H+pad, pad:W+pad] = img
+    
+    #kernel
+    kernel = np.zeros((k_size, k_size))
+    #kernelの中心を(x,y)=(0,0)としてガウス分布入れる
+    for i in range(k_size):
+        y = i - pad
+        for j in range(k_size):
+            x = j - pad
+            kernel[i, j] = np.exp(- (x ** 2 + y ** 2) / (2 * s ** 2)) / (2 * np.pi * s ** 2)
+    
+    #kernelを正規化(値の合計=1にする)
+    kernel /= kernel.sum()
+
+    #filtering
+    out = np.zeros((H+pad*2, W+pad*2, C)).astype(np.float32)
+
+    for y in range(pad, H+pad):
+        for x in range(pad, W+pad):
+            for c in range(C):
+                out[y, x, c] = np.sum(img_pad[y-pad:y-pad+k_size, x-pad:x-pad+k_size, c] * kernel)
+    
+    #padding除去
+    out = out[pad:H+pad, pad:W+pad]
+
+    return out
+
+
+img = cv2.imread("imori_noise.jpg").astype(np.float32)
+
+out = gaussian_filter(img, k_size=3, s=1.3).astype(np.uint8)
+
+cv2.imwrite(r"out\out_09.jpg", out)
+cv2.imshow("", out)
+cv2.waitKey(0)
+cv2.destroyAllWindows
