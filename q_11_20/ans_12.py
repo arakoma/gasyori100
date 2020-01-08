c4d57ee6cb0e5f162e2b0aeb0e662a6801dcafbb
arakoma <arakoma555@gmail.com>
2020-01-09 03:46:44 +0900

    add 11 12 13

--- q_11_20/ans_12.py
+++ q_11_20/ans_12.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def mean_filter(img_, k_size):
+def motion_filter(img_, k_size):
     if len(img_.shape) == 2:
         img = img_.copy()
         img = np.expand_dims(img, -1)
@@ -15,6 +15,9 @@ def mean_filter(img_, k_size):
     #padding
     img_pad = np.zeros((H+pad*2, W+pad*2, C)).astype(np.float32)
     img_pad[pad:H+pad, pad:W+pad] = img
+
+    #kernel
+    kernel = np.identity(k_size) / k_size
     
     #filtering
     out = np.zeros((H+pad*2, W+pad*2, C)).astype(np.float32)
@@ -22,7 +25,7 @@ def mean_filter(img_, k_size):
     for y in range(H):
         for x in range(W):
             for c in range(C):
-                out[y+pad, x+pad, c] = np.mean(img_pad[y:y+k_size, x:x+k_size, c])
+                out[y+pad, x+pad, c] = np.sum(img_pad[y:y+k_size, x:x+k_size, c] * kernel)
     
     #padding除去
     out = out[pad:H+pad, pad:W+pad]
@@ -32,9 +35,9 @@ def mean_filter(img_, k_size):
 
 img = cv2.imread("imori.jpg").astype(np.float32)
 
-out = mean_filter(img, k_size=3).astype(np.uint8)
+out = motion_filter(img, k_size=3).astype(np.uint8)
 
-cv2.imwrite(r"out\out_11.jpg", out)
+cv2.imwrite(r"out\out_12.jpg", out)
 cv2.imshow("", out)
 cv2.waitKey(0)
 cv2.destroyAllWindows
--- q_11_20/ans_13.py
+++ q_11_20/ans_13.py
@@ -2,7 +2,17 @@ import cv2
 import numpy as np
 
 
-def mean_filter(img_, k_size):
+def BGR2gray(img):
+    b = img[:, :, 0].copy()
+    g = img[:, :, 1].copy()
+    r = img[:, :, 2].copy()
+
+    gray = 0.2126 * r + 0.7152 * g + 0.0722 * b
+
+    return gray
+
+
+def maxmin_filter(img_, k_size):
     if len(img_.shape) == 2:
         img = img_.copy()
         img = np.expand_dims(img, -1)
@@ -15,14 +25,15 @@ def mean_filter(img_, k_size):
     #padding
     img_pad = np.zeros((H+pad*2, W+pad*2, C)).astype(np.float32)
     img_pad[pad:H+pad, pad:W+pad] = img
-    
+
     #filtering
     out = np.zeros((H+pad*2, W+pad*2, C)).astype(np.float32)
     
     for y in range(H):
         for x in range(W):
             for c in range(C):
-                out[y+pad, x+pad, c] = np.mean(img_pad[y:y+k_size, x:x+k_size, c])
+                region = img_pad[y:y+k_size, x:x+k_size, c]
+                out[y+pad, x+pad, c] = np.max(region) - np.min(region)
     
     #padding除去
     out = out[pad:H+pad, pad:W+pad]
@@ -32,9 +43,11 @@ def mean_filter(img_, k_size):
 
 img = cv2.imread("imori.jpg").astype(np.float32)
 
-out = mean_filter(img, k_size=3).astype(np.uint8)
+img_gray = BGR2gray(img)
 
-cv2.imwrite(r"out\out_11.jpg", out)
+out = maxmin_filter(img_gray, k_size=3).astype(np.uint8)
+
+cv2.imwrite(r"out/out_13.jpg", out)
 cv2.imshow("", out)
 cv2.waitKey(0)
 cv2.destroyAllWindows
